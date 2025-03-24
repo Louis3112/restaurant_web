36d0960c9c79a46005bcfc04a43e472b793f99d0
Louis <corneliuslouis3112@gmail.com>
2025-03-24 22:00:42 +0700

    adding simple javascript to calc total food

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function handleSubmit() {
+    const total = calcTotal(); 
+    const formattedTotal = formatCurrency(total); 
+    printTotal(formattedTotal);
+    return false;
+}
+
+function calcTotal(){
+    const items = document.querySelectorAll('.food-item');
+    let total = 0;
+    items.forEach(item => {
+        const priceText = item.querySelector('.food-price').textContent;
+        const price = parseInt(priceText.replace(/[^\d]/g, '')) || 0; 
+        const quantity = parseInt(item.querySelector('.order-input').value) || 0;
+        
+        total += price * quantity;
+    });
+    return total;
+}
+
+function formatCurrency(amount) {
+    return new Intl.NumberFormat('id-ID', { 
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(amount);
+}
+
+function printTotal(formattedTotal) {
+    alert(`Your Total is ${formattedTotal} \nPlease wait for a moment`);
+}
+  
